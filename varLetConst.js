7d7dff0a16f40b3cc8f9f52fdea822005a068ee1
Michael Aylmer <michael.aylmer@yahoo.com>
2023-02-28 13:28:53 -0800

    let var and const rules

---
+++ varLetConst.js
@@ -0,0 +1,14 @@
+// answer 1
+const PI = 3.14
+// answer 2
+The difference between var and let is var is a function scope and let is a block scope and you 
+can redeclare var.
+// answer 3
+difference between var and const is const cannot be redeclare and const is scoped to whatever code
+block it is inside
+// answer 3
+the difference between let and const is let allows u to assign new varaible, const only lets you mutate
+the continents.
+// answer 4
+hosting is when u declare something is var even though its half was down the page of code it still is
+declared at the top and is undefined until you declare what is inside it.
